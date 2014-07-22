6fd9c9d21eb6a5e44a9efb3810b532b1a79bba55
Wille Marcel <wille@wille.blog.br>
2014-07-22 11:07:46 -0300

    login_required to view

--- cadastro/views.py
+++ cadastro/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
 from .models import Disciplina
 
 
+@login_required(login_url='/admin/')
 def relatorio(request, centro):
     disciplinas = Disciplina.objects.filter(docente__centro=centro) \
         .order_by('docente')
